e566a3883e8ff7a8955af9c8bccaa015d999f598
antor10655gh <creativefreelancerantor@gmail.com>
2022-07-31 00:45:36 +0600

    add json file and show data by using axios

--- src/components/SalesInvoiceTable.js
+++ src/components/SalesInvoiceTable.js
@@ -1,12 +1,55 @@
-import React, { useState } from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import DataTable from "react-data-table-component";
 
 const SalesInvoiceTable = () => {
+  const [invoices, setInvoices] = useState([]);
+
+  const getInvoices = async () => {
+    try {
+      const response = await axios.get("invoices.json");
+      setInvoices(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const columns = [
+    {
+      name: "Invoice ID",
+      selector: (row) => row.id,
+    },
+    {
+      name: "Date",
+      selector: (row) => row.date,
+    },
+    {
+      name: "Customer Name",
+      selector: (row) => row.customer,
+    },
+    {
+      name: "Payable Amount",
+      selector: (row) => row.payableAmount,
+    },
+    {
+      name: "Paid Amount",
+      selector: (row) => row.paidAmount,
+    },
+    {
+      name: "Due Amount",
+      selector: (row) => row.dueAmount,
+    },
+  ];
+
+  useEffect(() => {
+    getInvoices();
+  }, []);
+
   return (
     <div className="container">
       <div className="row row-cols-1 row-cols-lg-2">
         <div className="col">
-          <DataTable />
+          <DataTable columns={columns} data={invoices} pagination />
         </div>
         <div className="col">
           <h1>Hi</h1>
