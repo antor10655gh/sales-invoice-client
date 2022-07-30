a99854507576d339428b826d55cbaba524d2f11f
antor10655gh <creativefreelancerantor@gmail.com>
2022-07-31 01:21:27 +0600

    add search option

--- src/components/SalesInvoiceTable.js
+++ src/components/SalesInvoiceTable.js
@@ -4,11 +4,14 @@ import DataTable from "react-data-table-component";
 
 const SalesInvoiceTable = () => {
   const [invoices, setInvoices] = useState([]);
+  const [search, setSearch] = useState("");
+  const [filterInvoices, setFilterInvoices] = useState([]);
 
   const getInvoices = async () => {
     try {
       const response = await axios.get("invoices.json");
       setInvoices(response.data);
+      setFilterInvoices(response.data);
     } catch (error) {
       console.log(error);
     }
@@ -39,27 +42,58 @@ const SalesInvoiceTable = () => {
       name: "Due Amount",
       selector: (row) => row.dueAmount,
     },
+    {
+      name: "Action",
+      cell: (row) => (
+        <button
+          className="btn btn-sm btn-primary"
+          onClick={() => alert(row.id)}
+        >
+          Delete
+        </button>
+      ),
+    },
   ];
 
   useEffect(() => {
     getInvoices();
   }, []);
 
+  useEffect(() => {
+    const result = invoices.filter((invoice) => {
+      return invoice.customer.toLowerCase().match(search.toLowerCase());
+    });
+
+    setFilterInvoices(result);
+  }, [search]);
+
   return (
     <div className="container">
-      <div className="row row-cols-1 row-cols-lg-2">
-        <div className="col">
+      <div className="row row-cols-1">
+        <div className="col col-lg-8">
           <DataTable
-            title={"Sales"}
+            title={<h1>Sales</h1>}
             columns={columns}
-            data={invoices}
+            data={filterInvoices}
             pagination
             fixedHeader
-            fixedHeaderScrollHeight="500px"
+            fixedHeaderScrollHeight="450px"
             selectableRows
+            selectableRowsHighlight
+            highlightOnHover
+            subHeader
+            subHeaderComponent={
+              <input
+                type="text"
+                placeholder="Search"
+                className="w-50 form-control"
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+              />
+            }
           />
         </div>
-        <div className="col">
+        <div className="col col-lg-4">
           <h1>Hi</h1>
         </div>
       </div>
